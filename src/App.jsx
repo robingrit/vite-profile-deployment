a577dbe5e4adda9387e9f680c0f5897d229c2656
zorpid <91528697+robingrit@users.noreply.github.com>
2022-12-30 06:00:24 +0100

    gh
    gh

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./Styles/Home.scss";
-import About from "./components/About";
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import Skills from "./components/Skills";
-import Footer from "./components/Footer";
-import Projects from "./components/Projects";
+import About from "./modules/About";
+import NavBar from "./modules/NavBar";
+import Home from "./modules/Home";
+import Skills from "./modules/Skills";
+import Footer from "./modules/Footer";
+import Projects from "./modules/Projects";
 
 function App() {
   const [navstate, setNavState] = useState("Home");
