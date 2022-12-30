9203ab071df0be8bf9d3f35691ea3a6977a5a477
zorpid <91528697+robingrit@users.noreply.github.com>
2022-12-30 18:08:58 +0100

    final
    final

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-vite-profile-deployment
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
-});
+  plugins: [react()]
+})
